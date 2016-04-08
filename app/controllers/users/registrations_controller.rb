79c034edf4e89f8cb2587253f8c1f132ee1183cf
volandy <volandy14@gmail.com>
2016-04-08 13:32:07 +0000

    permitted name prior to signup

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -4,7 +4,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:account_update).push(:name, :email, :password)
+    devise_parameter_sanitizer.for(:sign_up).push(:name, :email, :password)
   end
 end
 # before_filter :configure_sign_up_params, only: [:create]
